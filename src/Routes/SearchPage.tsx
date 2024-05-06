69b5ca490f7aa276e62889a184f52dd78fa0c96b
Sebastian <selfishworm117@gmail.com>
2024-05-06 02:42:06 +0200

    ok i get stuff now

--- src/Routes/SearchPage.tsx
+++ src/Routes/SearchPage.tsx
@@ -1,32 +1,18 @@
 import { useContext, useEffect, useState } from "react";
 import { BookContext } from "@/context/BookContext";
 import axios from "axios";
+import { useFetch } from "@/hooks/useFetch";
 
 export const SearchPage = () => {
   const { searchTerm } = useContext(BookContext);
-  // const [searchResult] = useFetch(`https://openlibrary.org/search.json?title=${searchTerm}`);
-  const [result, setResult] = useState<any>(null);
-
-  useEffect(() => {
-    console.log(searchTerm);
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`https://openlibrary.org/search.json?title=${searchTerm}`);
-        setResult(response.data);
-      } catch (error) {
-        console.error("Error with fetching...", error);
-      }
-    };
-
-    fetchData();
-  }, [searchTerm]);
+  const searchResult = useFetch(`https://openlibrary.org/search.json?title=${searchTerm}`);
 
   return (
     <div>
-      {result ? (
-        result.docs.map((book: any) => (
+      {searchResult ? (
+        searchResult.docs.map((book: any) => (
           <div key={book.key}>
-            <h2>{book.title}</h2>
+            <h2>{book.oclc ? book.oclc[1] : "none"}</h2>
           </div>
         ))
       ) : (
