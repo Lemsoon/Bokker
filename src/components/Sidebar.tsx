fb87664f99b31428f923ad434aa83553f6a430c5
Sebastian <seblhog@gmail.com>
2024-05-10 11:35:31 +0200

    Sidebar styled & works

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,31 @@
 import { BookContext } from "@/context/BookContext";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
   const { favoriteBooksInfo } = useContext(BookContext);
-
+  console.log(favoriteBooksInfo);
   return (
-    <div className="h-[51rem]">
+    <div className=" overflow-hidden">
       <h1 className="text-4xl underline text-center tracking-[.3rem]">Your Favorites</h1>
       {favoriteBooksInfo.length > 0 ? (
-        <ul>
+        <ul className="overflow-y-scroll overflow-hidden max-h-[80vh]">
           {favoriteBooksInfo.map((book, i) => {
-            return <li key={i}>{book.title}</li>;
+            return (
+              <Link to={`/book${book.key}`}>
+                <li
+                  key={i}
+                  className="text-center bg-red-100 mt-2 p-2 flex justify-center items-center border-black border-2 hover:scale-[1.01] duration-200"
+                >
+                  <img
+                    src={book.covers ? `https://covers.openlibrary.org/b/id/${book.covers[0]}-M.jpg` : ""}
+                    alt={`cover image`}
+                    className=" max-w-12 h-full border-2 border-black"
+                  />
+                  <div>{book.title}</div>
+                </li>
+              </Link>
+            );
           })}
         </ul>
       ) : (
--- src/context/BookContext.ts
+++ src/context/BookContext.ts
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 
 export type FavoriteBookType = {
+  covers: string[];
   title: string;
   avgRating: number;
   key: string;
